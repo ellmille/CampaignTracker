ec833c464871e3a7a71346cd67782348643b7f39
ellmille <ellelynn98@gmail.com>
2018-03-07 12:53:15 -0600

    added class templates

---
+++ app/src/main/java/com/elle/campaigntracker/data/Character.java
@@ -0,0 +1,12 @@
+package com.elle.campaigntracker.data;
+
+import android.databinding.BaseObservable;
+
+/**
+ * observable object representing a character
+ */
+
+public class Character extends BaseObservable {
+    private String name, playerClass, background;
+    private int level;
+}
---
+++ app/src/main/java/com/elle/campaigntracker/data/repo/DummyRepo.java
@@ -0,0 +1,8 @@
+package com.elle.campaigntracker.data.repo;
+
+/**
+ * Character sheet pre-sqlite setup
+ */
+
+public class DummyRepo {
+}
