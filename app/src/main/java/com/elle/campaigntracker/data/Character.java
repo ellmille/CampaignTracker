fff3c160ee4ffddd71d03ce7bf83d74e2cfafd32
ellmille <ellelynn98@gmail.com>
2018-03-07 13:03:42 -0600

    added stats

--- app/src/main/java/com/elle/campaigntracker/data/Character.java
+++ app/src/main/java/com/elle/campaigntracker/data/Character.java
@@ -9,4 +9,309 @@ import android.databinding.BaseObservable;
 public class Character extends BaseObservable {
     private String name, playerClass, background;
     private int level;
+    private int xpTotal, xpToNextLevel;
+    private Alignment alignment;
+
+    private int armorClass, initiative, speed;
+    private int maxHp, currentHp;
+
+    private int inspiration, proficiencyBonus;
+    //saving throws
+    private int strength, dexterity, constitution, intelligence;
+    private int wisdom, charisma;
+    //skills
+    private int acrobatics, animalHandling, arcana, athletics;
+    private int deception, history, intimidation, medicine, nature;
+    private int perception, performance, religion, sleightOfHand;
+    private int stealth, survival;
+
+    private int passiveWisdom;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlayerClass() {
+        return playerClass;
+    }
+
+    public void setPlayerClass(String playerClass) {
+        this.playerClass = playerClass;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getXpTotal() {
+        return xpTotal;
+    }
+
+    public void setXpTotal(int xpTotal) {
+        this.xpTotal = xpTotal;
+    }
+
+    public int getXpToNextLevel() {
+        return xpToNextLevel;
+    }
+
+    public void setXpToNextLevel(int xpToNextLevel) {
+        this.xpToNextLevel = xpToNextLevel;
+    }
+
+    public Alignment getAlignment() {
+        return alignment;
+    }
+
+    public void setAlignment(Alignment alignment) {
+        this.alignment = alignment;
+    }
+
+    public int getArmorClass() {
+        return armorClass;
+    }
+
+    public void setArmorClass(int armorClass) {
+        this.armorClass = armorClass;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getMaxHp() {
+        return maxHp;
+    }
+
+    public void setMaxHp(int maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
+    }
+
+    public void setCurrentHp(int currentHp) {
+        this.currentHp = currentHp;
+    }
+
+    public int getInspiration() {
+        return inspiration;
+    }
+
+    public void setInspiration(int inspiration) {
+        this.inspiration = inspiration;
+    }
+
+    public int getProficiencyBonus() {
+        return proficiencyBonus;
+    }
+
+    public void setProficiencyBonus(int proficiencyBonus) {
+        this.proficiencyBonus = proficiencyBonus;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+
+    public int getAcrobatics() {
+        return acrobatics;
+    }
+
+    public void setAcrobatics(int acrobatics) {
+        this.acrobatics = acrobatics;
+    }
+
+    public int getAnimalHandling() {
+        return animalHandling;
+    }
+
+    public void setAnimalHandling(int animalHandling) {
+        this.animalHandling = animalHandling;
+    }
+
+    public int getArcana() {
+        return arcana;
+    }
+
+    public void setArcana(int arcana) {
+        this.arcana = arcana;
+    }
+
+    public int getAthletics() {
+        return athletics;
+    }
+
+    public void setAthletics(int athletics) {
+        this.athletics = athletics;
+    }
+
+    public int getDeception() {
+        return deception;
+    }
+
+    public void setDeception(int deception) {
+        this.deception = deception;
+    }
+
+    public int getHistory() {
+        return history;
+    }
+
+    public void setHistory(int history) {
+        this.history = history;
+    }
+
+    public int getIntimidation() {
+        return intimidation;
+    }
+
+    public void setIntimidation(int intimidation) {
+        this.intimidation = intimidation;
+    }
+
+    public int getMedicine() {
+        return medicine;
+    }
+
+    public void setMedicine(int medicine) {
+        this.medicine = medicine;
+    }
+
+    public int getNature() {
+        return nature;
+    }
+
+    public void setNature(int nature) {
+        this.nature = nature;
+    }
+
+    public int getPerception() {
+        return perception;
+    }
+
+    public void setPerception(int perception) {
+        this.perception = perception;
+    }
+
+    public int getPerformance() {
+        return performance;
+    }
+
+    public void setPerformance(int performance) {
+        this.performance = performance;
+    }
+
+    public int getReligion() {
+        return religion;
+    }
+
+    public void setReligion(int religion) {
+        this.religion = religion;
+    }
+
+    public int getSleightOfHand() {
+        return sleightOfHand;
+    }
+
+    public void setSleightOfHand(int sleightOfHand) {
+        this.sleightOfHand = sleightOfHand;
+    }
+
+    public int getStealth() {
+        return stealth;
+    }
+
+    public void setStealth(int stealth) {
+        this.stealth = stealth;
+    }
+
+    public int getSurvival() {
+        return survival;
+    }
+
+    public void setSurvival(int survival) {
+        this.survival = survival;
+    }
+
+    public int getPassiveWisdom() {
+        return passiveWisdom;
+    }
+
+    public void setPassiveWisdom(int passiveWisdom) {
+        this.passiveWisdom = passiveWisdom;
+    }
 }
