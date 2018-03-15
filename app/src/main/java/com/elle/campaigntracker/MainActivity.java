58baa0b223ec5304b0a59138a98fa553250d139e
Elle Miller <elle.miller.a@gmail.com>
2018-03-15 18:15:02 -0500

    merge

--- app/src/main/java/com/elle/campaigntracker/MainActivity.java
+++ app/src/main/java/com/elle/campaigntracker/MainActivity.java
@@ -1,20 +1,17 @@
 package com.elle.campaigntracker;
 
-import android.content.Context;
-import android.content.Intent;
-import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.elle.campaigntracker.data.character.PlayerCharacter;
-import com.elle.campaigntracker.data.repo.DummyRepo;
-import com.elle.campaigntracker.databinding.ActivityMainBinding;
-import com.elle.campaigntracker.view.PlayerTurnActivity;
-import com.elle.campaigntracker.view.UpdateHealthFragment;
-
-public class MainActivity extends AppCompatActivity {
-    private PlayerCharacter character;
-    ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //todo: add activity contract
     //todo: add menu --> option to view character sheet, start turn, take damage, heal
@@ -22,32 +19,68 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        //todo: remove/refactor
-        new DummyRepo();
-        character = DummyRepo.getRex();
-
-        //get last used character
-        binding.setHandler(new Handler());
-        binding.setCharacter(character);
-
+        setContentView(R.layout.activity_main);
+        //setup tool bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //set up drawer
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        //set up nav drawer
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private Context getContext(){
-        return this;
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
-    public class Handler {
-        public void updateHealth(boolean isHealing){
-            UpdateHealthFragment updateHealthFragment = UpdateHealthFragment.newInstance(isHealing);
-            updateHealthFragment.show(getSupportFragmentManager(), "dialog");
-        }
-        public void startTurn(PlayerCharacter character){
-            System.out.println(character.getCharacterName());
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-            Intent intent = new Intent(getContext(), PlayerTurnActivity.class);
-            getContext().startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_full_sheet) {
+            // Handle the camera action
+        } else if (id == R.id.nav_inventory) {
+
+        } else if (id == R.id.nav_log) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
