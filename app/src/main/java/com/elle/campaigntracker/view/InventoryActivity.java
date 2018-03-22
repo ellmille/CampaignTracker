5e79358ba144384ab6d74dcf031b39a4d72295db
ellmille <ellelynn98@gmail.com>
2018-03-22 13:48:28 -0500

    added inventory activity

--- app/src/main/java/com/elle/campaigntracker/view/InventoryActivity.java
+++ app/src/main/java/com/elle/campaigntracker/view/InventoryActivity.java
@@ -1,15 +1,21 @@
 package com.elle.campaigntracker.view;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.elle.campaigntracker.R;
+import com.elle.campaigntracker.viewmodel.InventoryViewModel;
 
 public class InventoryActivity extends AppCompatActivity {
+    private InventoryViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+        //todo: add recycler view, binding and adapter
+        //set view model
+        viewModel = ViewModelProviders.of(this).get(InventoryViewModel.class);
     }
 }
