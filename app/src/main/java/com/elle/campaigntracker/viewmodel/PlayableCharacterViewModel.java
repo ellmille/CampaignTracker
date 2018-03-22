3b967716da7ddb0e2c95d95285fa5a724db4f6c8
ellmille <ellelynn98@gmail.com>
2018-03-22 13:05:02 -0500

    extend AndroidViewModel

--- app/src/main/java/com/elle/campaigntracker/viewmodel/PlayableCharacterViewModel.java
+++ app/src/main/java/com/elle/campaigntracker/viewmodel/PlayableCharacterViewModel.java
@@ -1,30 +1,34 @@
 package com.elle.campaigntracker.viewmodel;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
 
-import com.elle.campaigntracker.data.DummyRepo;
+import com.elle.campaigntracker.App;
+import com.elle.campaigntracker.Repo;
 import com.elle.campaigntracker.data.model.PlayableCharacter;
 
 /**
  * Prepares data for the UI
  */
 
-public class PlayableCharacterViewModel extends ViewModel {
-    private MutableLiveData<PlayableCharacter> playableCharacter;
+public class PlayableCharacterViewModel extends AndroidViewModel {
+    private final LiveData<PlayableCharacter> observableCharacter;
+    private final Repo repo;
+    public ObservableField<PlayableCharacter> playableCharacter = new ObservableField<>();
 
-    public LiveData<PlayableCharacter> getPlayableCharacter(){
-        if(playableCharacter == null){
-            playableCharacter = new MutableLiveData<>();
-            loadCharacter();
-        }
-        return playableCharacter;
+    public PlayableCharacterViewModel(Application application){
+        super(application);
+        this.repo = ((App) application).getRepo();
+        this.observableCharacter = repo.getCharacter();
     }
 
-    private void loadCharacter() {
-        //todo: get ID from last shared prefs
-        DummyRepo dummyRepo = new DummyRepo();
-        playableCharacter.setValue(dummyRepo.getPlayableChar());
+    public LiveData<PlayableCharacter> getObservableCharacter(){
+        return observableCharacter;
+    }
+
+    public void setPlayableCharacter(PlayableCharacter playableCharacter){
+        this.playableCharacter.set(playableCharacter);
     }
 }
