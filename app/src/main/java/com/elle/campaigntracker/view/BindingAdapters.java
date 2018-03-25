96c7e5a543744e8538810d83a74b77bcdb33a7b1
Elle Miller <elle.miller.a@gmail.com>
2018-03-25 18:21:30 -0500

    added bindings

--- app/src/main/java/com/elle/campaigntracker/view/BindingAdapters.java
+++ app/src/main/java/com/elle/campaigntracker/view/BindingAdapters.java
@@ -21,27 +21,29 @@ import java.util.Locale;
 public class BindingAdapters {
     private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMMM.dd - yyyy", Locale.getDefault());
 
-//    @BindingAdapter(value = "intToTextAttrChanged")
-//    public static void setListener(EditText editText, final InverseBindingListener listener){
-//        if(listener != null){
-//            editText.addTextChangedListener(new TextWatcher() {
-//                @Override
-//                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//                }
-//
-//                @Override
-//                public void onTextChanged(CharSequence s, int start, int before, int count) {
-//
-//                }
-//
-//                @Override
-//                public void afterTextChanged(Editable s) {
-//                    listener.onChange();
-//                }
-//            });
-//        }
-//    }
+    @BindingAdapter(value = "intToTextAttrChanged")
+    public static void setListener(EditText editText, final InverseBindingListener listener){
+        if(listener != null){
+            editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if(!editText.isFocused()){
+                        listener.onChange();
+                    }
+                }
+            });
+        }
+    }
 
     @BindingAdapter("intToText")
     public static void setIntToText(EditText view, int number){
@@ -56,10 +58,10 @@ public class BindingAdapters {
         view.setText(simpleDateFormat.format(date));
     }
 
-//    @InverseBindingAdapter(attribute = "intToText")
-//    public static int getNumber(EditText view){
-//        return Integer.parseInt(view.getText().toString());
-//    }
+    @InverseBindingAdapter(attribute = "intToText")
+    public static int getNumber(EditText view){
+        return Integer.parseInt(view.getText().toString());
+    }
 
     @BindingAdapter("visibleGone")
     public static void showHide(View view, boolean show) {
