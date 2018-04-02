384618be2974cbd7e6c1f856d208040db9ac086a
ellmille <ellelynn98@gmail.com>
2018-04-02 13:36:21 -0500

    update conversions

--- app/src/main/java/com/elle/campaigntracker/data/model/Money.java
+++ app/src/main/java/com/elle/campaigntracker/data/model/Money.java
@@ -26,7 +26,7 @@ public class Money {
 
     //    1 gold = 10 silver, 1 silver = 10 copper, 1 electrum = 5 silver, 1 platinum = 10 gold
     @Ignore
-    private final int goldSilver = 10, silverCopper = 10, platinumGold = 10, electrumSilver = 5;
+    private final int goldSilver = 10, silverCopper = 10, platinumGold = 10, electrumSilver = 5, goldElectrum = 2;
 
     @Ignore
     public Money(int charId, int gold, int silver, int copper, int electrum, int platinum){
@@ -149,23 +149,50 @@ public class Money {
         }
     }
 
-    public void spendGold(int amount){
-        if(gold > amount){
+    void spendGold(int amount) {
+        if (gold > amount) {
             setGold(gold - amount);
             return;
         }
-
+        if (electrum < (goldElectrum) && platinum == 0 && copper < (silverCopper * goldSilver) && silver < goldSilver) {
+            return;
+        }
+        if ((electrum / goldElectrum) + (copper / (silverCopper * goldSilver)) + (silver / goldSilver) +
+                (platinum * platinumGold) + gold > amount) {
+            while(gold < amount){
+                if(copper >= (silverCopper * goldSilver)){
+                    setGold(gold + 1);
+                    setCopper(copper - (silverCopper * goldSilver));
+                    break;
+                }
+                if(silver >= goldSilver){
+                    setGold(gold + 1);
+                    setSilver(silver - goldSilver);
+                    break;
+                }
+                if(electrum >= goldElectrum){
+                    setGold(gold + 1);
+                    setElectrum(electrum - goldElectrum);
+                    break;
+                }
+                if(platinum > 0){
+                    setGold(gold + platinumGold);
+                    setPlatinum(platinum - 1);
+                }
+            }
+            setGold(gold - amount);
+        }
     }
 
-    public void spendSilver(int amount){
+    void spendSilver(int amount){
         if(silver >= amount){
             setSilver(silver - amount);
             return;
         }
-        if(electrum == 0 && platinum == 0 && copper == 0 && gold == 0){
+        if(electrum == 0 && platinum == 0 && copper < silverCopper && gold == 0){
             return;
         }
-        if((gold * goldSilver) + (electrum * electrumSilver) + (copper / silverCopper) + (platinum * platinumGold * goldSilver) > amount){
+        if((gold * goldSilver) + (electrum * electrumSilver) + (copper / silverCopper) + (platinum * platinumGold * goldSilver) + silver> amount){
             while (silver < amount){
                 //convert copper to silver
                 if(copper >= silverCopper){
@@ -198,6 +225,34 @@ public class Money {
             setCopper(copper - amount);
             return;
         }
+        if(electrum == 0 && platinum == 0 && silver == 0 && gold == 0){
+            return;
+        }
+        if((gold * goldSilver * silverCopper) + (electrum * electrumSilver * silverCopper) +
+                (platinum * platinumGold * goldSilver * silverCopper) + copper> amount){
+            while(copper < amount){
+                if(silver > 0){
+                    setCopper(copper + silverCopper);
+                    setSilver(silver - 1);
+                    continue;
+                }
+                if(gold > 0){
+                    setSilver(silver + goldSilver);
+                    setGold(gold - 1);
+                    continue;
+                }
+                if(electrum > 0){
+                    setSilver(silver + electrumSilver);
+                    setElectrum(electrum - 1);
+                    continue;
+                }
+                if(platinum > 0){
+                    setGold(gold + platinumGold);
+                    setPlatinum(platinum - 1);
+                }
+            }
+            setCopper(copper - amount);
+        }
     }
 
     public void spendElectrum(int amount){
@@ -205,27 +260,68 @@ public class Money {
             setElectrum(electrum - amount);
             return;
         }
+        if(silver == 0 && platinum == 0 && copper == 0 && gold == 0){
+            return;
+        }
+        if(gold * goldElectrum + (platinum * (platinumGold * goldElectrum)) + (silver / electrumSilver) +
+                (copper / (silverCopper * electrumSilver)) >= amount){
+            while(electrum < amount){
+                if(silver >= electrumSilver){
+                    setSilver(silver - electrumSilver);
+                    setElectrum(electrum + 1);
+                    continue;
+                }
+                if(copper >= electrumSilver * silverCopper){
+                    setCopper(copper - (electrumSilver * silverCopper));
+                    setElectrum(electrum + 1);
+                    continue;
+                }
+                if(gold > 0){
+                    setGold(gold - 1);
+                    setElectrum(electrum + goldElectrum);
+                    continue;
+                }
+                if(platinum > 0){
+                    setPlatinum(platinum - 1);
+                    setGold(gold + platinumGold);
+                }
+            }
+            setElectrum(electrum - amount);
+        }
     }
 
-    public boolean spendPlatinum(int amount){
+    public void spendPlatinum(int amount){
         if(platinum > amount){
             setPlatinum(platinum - amount);
-            return true;
+            return;
         }
         if(electrum == 0 && silver == 0 && copper == 0 && gold == 0){
-            return false;
+            return;
         }
-        while (amount > 0){
-            //todo: check we have enough before subtracting
-            if(gold > platinumGold){
-                setGold(gold - 10);
-                amount--;
+        if ((gold / platinumGold) + (copper / (silverCopper * goldSilver * platinumGold)) + (silver / (goldSilver * platinumGold)) +
+                (electrum / (goldElectrum * platinumGold)) + platinum > amount) {
+            while (platinum < amount){
+                if(copper >= (silverCopper * goldSilver * platinumGold)){
+                    setCopper(copper - (silverCopper * goldSilver * platinumGold));
+                    setPlatinum(platinum + 1);
+                    continue;
+                }
+                if(silver >= (goldSilver * platinumGold)){
+                    setSilver(silver - (goldSilver * platinumGold));
+                    setPlatinum(platinum + 1);
+                    continue;
+                }
+                if(gold >= platinumGold){
+                    setGold(gold - platinumGold);
+                    setPlatinum(platinum + 1);
+                    continue;
+                }
+                if(electrum >= (goldElectrum * platinumGold)){
+                    setElectrum(electrum - (goldElectrum * platinumGold));
+                    setPlatinum(platinum + 1);
+                }
             }
-            if(silver > platinumGold * goldSilver){
-                setSilver(silver - 100);
-                amount--;
-            }
+            setPlatinum(platinum - amount);
         }
-        return true;
     }
 }
