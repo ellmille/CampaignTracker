ecdc36328497ad38950502252d6c2abe3b923197
ellmille <ellelynn98@gmail.com>
2018-04-06 12:55:37 -0500

    added fragment for abilities

---
+++ app/src/main/java/com/elle/campaigntracker/view/character/AbilitiesFragment.java
@@ -0,0 +1,30 @@
+package com.elle.campaigntracker.view.character;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.elle.campaigntracker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AbilitiesFragment extends Fragment {
+
+
+    public AbilitiesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_abilities, container, false);
+    }
+
+}
