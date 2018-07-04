21fadc8bfc6aaaca0ef6e0916cb1342f60d40541
Elle Miller <elle.miller.a@gmail.com>
2018-07-04 18:06:11 -0500

    added view model for wallet

--- app/src/main/java/com/elle/campaigntracker/view/character/HpFragment.java
+++ app/src/main/java/com/elle/campaigntracker/view/character/HpFragment.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,6 +18,8 @@ import com.elle.campaigntracker.data.model.PcStats;
 import com.elle.campaigntracker.databinding.FragmentHpBinding;
 import com.elle.campaigntracker.view.callback.DeathSavingCallback;
 import com.elle.campaigntracker.viewmodel.CharacterSheetViewModel;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,16 +35,16 @@ public class HpFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        viewModel = ViewModelProviders.of(getActivity()).get(CharacterSheetViewModel.class);
+        viewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(CharacterSheetViewModel.class);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_hp, container, false);
         binding.setHitDieString(viewModel.getHitDie());
         binding.setCallback(deathSavingCallback);
-        viewModel.getObservableStats().observe(getActivity(), new Observer<PcStats>() {
+        viewModel.getObservableStats().observe(Objects.requireNonNull(getActivity()), new Observer<PcStats>() {
             @Override
             public void onChanged(@Nullable PcStats pcStats) {
                 binding.setStats(pcStats);
---
+++ app/src/main/java/com/elle/campaigntracker/viewmodel/WalletViewModel.java
@@ -0,0 +1,32 @@
+package com.elle.campaigntracker.viewmodel;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+
+import com.elle.campaigntracker.data.model.Money;
+
+public class WalletViewModel extends ViewModel {
+    LiveData<Money> liveWallet;
+
+    private WalletViewModel(Money money){
+
+    }
+
+    public static class Factory extends ViewModelProvider.NewInstanceFactory {
+        private Money money;
+
+        public Factory(Money money) {
+            this.money = money;
+          //  mRepository = ((BasicApp) application).getRepository();
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            //noinspection unchecked
+            return (T) new WalletViewModel(money);
+        }
+    }
+}
